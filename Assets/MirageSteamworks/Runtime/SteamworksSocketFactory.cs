377cdecd69daf4e313ed45634bf3f4b6d5f5bffa
James Frowen <jamesfrowendev@gmail.com>
2024-04-02 15:19:33 +0100

    template SteamworksSocketFactory

--- Assets/MirageSteamworks/Runtime/SteamworksSocketFactory.cs
+++ Assets/MirageSteamworks/Runtime/SteamworksSocketFactory.cs
@@ -1,8 +1,30 @@
-using Mirage;
+using Mirage.SocketLayer;
 
-namespace Mirage.SteamworksSocket {
-    public class SteamworksSocketFactory : SocketFactory {
+namespace Mirage.SteamworksSocket
+{
+    public class SteamworksSocketFactory : SocketFactory
+    {
+        public override int MaxPacketSize => throw new System.NotImplementedException();
 
+        public override ISocket CreateServerSocket()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override ISocket CreateClientSocket()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override IEndPoint GetBindEndPoint()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override IEndPoint GetConnectEndPoint(string address = null, ushort? port = null)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
 
